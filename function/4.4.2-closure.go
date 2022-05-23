ca93fff932bad2a4078a135e9ea69037682066f4
puyu.chen <745690939@qq.com>
2022-05-23 23:09:01 +0800

    [ADD]闭包

---
+++ function/4.4.2-closure.go
@@ -0,0 +1,52 @@
+package function
+
+// 闭包（closure）是在词法上下文中引用了自由变量的函数，或者说是函数和其作用的环境的组合体。
+
+func closure(x int) func() {
+	return func() {
+		println(x)
+	}
+}
+
+func closure2(x int) func() {
+	println(&x)
+	return func() {
+		println(&x, x)
+	}
+}
+
+func closure3() []func() {
+	var s []func()
+
+	// for循环复用局部变量i，每次添加的匿名函数引用的是同一个变量。添加操作仅仅是将匿名函数放入列表，并未执行。
+	// 在外部执行这些函数时，它们读取的是环境变量i最后一次循环时的值。结果是2。
+	for i := 0; i < 2; i++ {
+		s = append(s, func() {
+			println(&i, i)
+		})
+	}
+	return s
+}
+
+func closure4() []func() {
+	var s []func()
+
+	// 解决上述问题的方法是每次用不同的环境变量或传参复制，让各自闭包环境各不相同。
+	for i := 0; i < 2; i++ {
+		x := i
+		s = append(s, func() {
+			println(&x, x)
+		})
+	}
+
+	return s
+}
+
+func closure5(x int) (func(), func()) {
+	return func() {
+			println(x)
+			x += 10 // 修改环境变量
+		}, func() {
+			println(x) // 显示环境变量
+		}
+}
---
+++ function/4.4.2-closure_test.go
@@ -0,0 +1,41 @@
+package function
+
+import "testing"
+
+func TestClosure(t *testing.T) {
+	// closure函数返回的匿名函数会引用上下文环境变量x。当该函数在其他函数中执行时，它仍然可正确读取x的值，这种现象叫做闭包。
+	f := closure(123)
+	f()
+}
+
+func TestClosure2(t *testing.T) {
+	f := closure2(0x100)
+	//f := closure2(123)
+	f()
+}
+
+func TestClosure3(t *testing.T) {
+
+	for _, f := range closure3() {
+		f()
+	}
+}
+
+func TestClosure4(t *testing.T) {
+	for _, f := range closure4() {
+		f()
+	}
+}
+
+func TestClosure5(t *testing.T) {
+	// 返回值是100， 110
+	// 多个匿名函数引用同一环境变量，会让事情变得更加复杂。任何的修改行为都会影响其他函数的取值，在并发模式下可能需要做同步处理。
+	a, b := closure5(100)
+	b()
+	a()
+	b()
+	a()
+	b()
+}
+
+// 闭包让我们不用传递参数就可读取或修改环境状态，但也要为此付出额外代价，对于性能要求较高的场合，需慎重使用。
