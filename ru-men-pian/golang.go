345eaeb96270f394ab037e3c692fd5198b0c9280
cpyou <745690939@qq.com>
2023-12-16 22:42:38 +0800

    fix:test

---
+++ ru-men-pian/golang.go
@@ -0,0 +1,16 @@
+package ru_men_pian
+
+func sample1() {
+	println(12334)
+	// 创建栈
+	//stack := make([]int, 0)
+	//// push压入
+	//stack = append(stack, 10)
+	//// pop弹出
+	//v := stack[len(stack)-1]
+	//println(v)
+	//stack = stack[:len(stack)-1]
+	//// 检查栈空
+	//println(len(stack))
+	//len(stack) == 0
+}
---
+++ ru-men-pian/golang_test.go
@@ -0,0 +1,20 @@
+package ru_men_pian
+
+import "testing"
+
+func Test_sample(t *testing.T) {
+	sample1()
+}
+
+func Test_sample1(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sample1()
+		})
+	}
+}
